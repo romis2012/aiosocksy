b3c3e5f067341f7b9ef1d3e1a61f9e5bf28a7df1
rsng <rsng@mail.ru>
2018-08-29 18:37:45 +0300

    Fix issue #4 aiohttp 3.4 compatibility

--- aiosocksy/connector.py
+++ aiosocksy/connector.py
@@ -1,9 +1,12 @@
 import aiohttp
 from aiohttp.client_exceptions import (
     ServerFingerprintMismatch,
-    certificate_errors,
     ssl_errors
 )
+try:
+    from aiohttp.client_exceptions import certificate_errors
+except ImportError:
+    from aiohttp.client_exceptions import cert_errors as certificate_errors
 
 from .errors import SocksConnectionError
 from .helpers import Socks4Auth, Socks5Auth, Socks4Addr, Socks5Addr
